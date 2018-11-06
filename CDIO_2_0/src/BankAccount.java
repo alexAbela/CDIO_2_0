753b6b37310aaabb6df515bad2296c4336f6e531
Nobert1 <s185031@student.dtu.dk>
2018-11-06 12:46:58 +0100

    Changes in bank account and two J unit test classes.

--- CDIO_2_0/src/BankAccount.java
+++ CDIO_2_0/src/BankAccount.java
@@ -1,42 +1,47 @@
 public class BankAccount {
 
     private int balance;
+    private int balanceMinvalue = 0;
 
     // Constructor, en simple klasse i denne omgang
 
     public BankAccount(int balance) {
 
-        this.balance = balance;
-
-    }
+            this.balance = balance;
+        }
 
 
     // Metode til når der skal penge ind på kontoen.
     public int deposit(int amount) {
 
         balance += amount;
-        System.out.println("Den nye balance er " + balance);
+        System.out.println("The new balance is " + balance);
         return balance;
 
     }
 
-    //Metode til når der skal penge du af kontoen.
+    //Metode til når penge skal ud af kontoen, den forsikrer også at kontoen ikke kan gå i minus.
 
     public int withdraw(int amount) {
 
         this.balance -= amount;
-        System.out.println("Den nye balance er " + this.balance);
-        return this.balance;
 
+        if (balance < 0) {
+            this.balance = 0;
+            System.out.println("The balance can't go below 0, your new balance is 0 ");
+            return this.balance;
+        } else if (balance > 0){
+            System.out.println("The funds have been withdrawn from your account, your new balance is " + getBalance());
+            return this.balance;
+        }
+        return this.balance;
     }
 
     // standard get metode der returnerer balancen.
     public int getBalance() {
-        return this.balance;
+            return this.balance;
 
     }
-    // standard set metode der returnerer balancen.
-
 
 }
 
